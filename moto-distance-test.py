c9c90bb43abd9f67c5071cf38e0dd763a281ffd3
往日。 <remini1998@users.noreply.github.com>
2018-07-11 16:40:37 +0800

    Update moto-distance-test.py

--- moto-distance-test.py
+++ moto-distance-test.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from gpioMotorsController import DeviceController
-if __name__ = "__main__":
+if __name__ == "__main__":
     dc = DeviceController()
     dc.test_pos_length()
     dc.test_dep_length()
